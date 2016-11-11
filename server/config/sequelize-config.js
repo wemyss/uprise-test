8701aa6c52d8cf5fb17d04ee7df8db28c264d49a
Sam Wemyss <samuel.wemyss@gmail.com>
2016-11-11 17:02:58 +1100

    updated config

--- server/config/sequelize-config.js
+++ server/config/sequelize-config.js
@@ -24,6 +24,28 @@ const config = {
         }
       }
     }
+  },
+  production: {
+    root: rootPath,
+    app: {
+      name: 'uprise-test'
+    },
+    port: 5432,
+    db: {
+      database: "d39vst2gr09ffs",
+      user: "dgnhfyehwqxnvy",
+      password: "4PScu-GuznMiAKgUoRrmBAOhMC",
+      options: {
+        host: 'ec2-54-243-202-84.compute-1.amazonaws.com',
+        dialect: 'postgres',
+
+        pool: {
+          max: 100,
+          min: 0,
+          idle: 10000
+        }
+      }
+    }
   }
 }
 
